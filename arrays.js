8ea7a0c99fa8750694a7fe8e192b951b779a4d29
tahseen447 <tahseenfatima1986@gmail.com>
2017-09-30 05:03:17 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,53 @@
+var chocolateBars =["snickers", "grand hundred", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element)
+{
+  var newArray = [element,...array];
+  return newArray;
+}
+function destructivelyAddElementToBeginningOfArray(array, element)
+{
+  array.unshift(element);
+  return array;
+}
+
+function addElementToEndOfArray(array, element)
+{
+  var newArray = [...array, element]
+  return newArray
+}
+
+function destructivelyAddElementToEndOfArray(array, element)
+{
+  array.push(element);
+  return array
+}
+
+function accessElementInArray(array, index)
+{
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array)
+{
+  array.shift()
+  return array
+}
+
+function removeElementFromBeginningOfArray(array)
+{
+  var element = array.slic(1)
+  return array
+}
+
+function destructivelyRemoveElementFromEndOfArray(array)
+{
+  array.pop()
+  return array
+}
+
+function removeElementFromEndOfArray(array)
+{
+  array.slice(0, array.length-1)
+  return array
+}
